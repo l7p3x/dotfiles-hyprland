841780f03dd8c3aa9f367003b89eb22007b5b038
l7p3x <pauloeicardovc@gmail.com>
2026-04-27 03:41:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/env bash
+set -e
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+SELF="$(basename "$0")"
 
+REPO_URL="https://www.github.com/googIyEYES/YAMIS.git"
+TMP_DIR="$(mktemp -d)"
+DEST="$HOME/.local/share/icons"
+
+# mover arquivos com segurança
 for f in "$SCRIPT_DIR"/*; do
+    [ -f "$f" ] || continue
+
     case "$(basename "$f")" in
-        .gitignore)
+        "$SELF"|.gitignore)
             continue
             ;;
     esac
-    mv "$f" ~/
+
+    mv "$f" "$HOME/"
 done
 
-git clone https://www.github.com/googIyEYES/YAMIS.git && cd YAMIS
-tar -xzvf monochrome-icon-theme.tar.gz -C "~/.local/share/icons/"
-sudo rm -r Yet-Another-Monochrome-Icon-Theme
+# clonar repo (shallow)
+git clone --depth 1 "$REPO_URL" "$TMP_DIR/YAMIS"
+
+# garantir destino
+mkdir -p "$DEST"
+
+# extrair
+tar -xzf "$TMP_DIR/YAMIS/monochrome-icon-theme.tar.gz" -C "$DEST"
+
+# limpeza
+rm -rf "$TMP_DIR"
