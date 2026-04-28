5d652e3988708b7822487716f6315d7002b73aab
l7p3x <pauloeicardovc@gmail.com>
2026-04-28 11:01:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,3 +32,26 @@ tar -xzf "$TMP_DIR/YAMIS/monochrome-icon-theme.tar.gz" -C "$DEST"
 
 # limpeza
 rm -rf "$TMP_DIR"
+
++++ install.sh (修改后)
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "Installing dotfiles..."
+
+rsync -av --exclude='install.sh' --exclude='.gitignore' --exclude='.git' "$SCRIPT_DIR/" "$HOME/"
+
+REPO_URL="https://github.com/googIyEYES/YAMIS.git"
+TMP_DIR=$(mktemp -d)
+ICON_DEST="$HOME/.local/share/icons"
+
+git clone --depth 1 "$REPO_URL" "$TMP_DIR/YAMIS"
+
+mkdir -p "$ICON_DEST"
+tar -xzf "$TMP_DIR/YAMIS/monochrome-icon-theme.tar.gz" -C "$ICON_DEST"
+
+rm -rf "$TMP_DIR"
+
+echo "Installation complete."
